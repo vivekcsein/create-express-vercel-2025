a4d46578e31033f9d527e5f88a3f3ade12e62a86
vivek <vivekcse.in@gmail.com>
2025-08-21 09:41:26 +0530

    local removal of files to rework

--- src/api/auth/auth.schemas.ts
+++
@@ -1,7 +0,0 @@
-import z from "zod";
-
-export const registerAuthSchema = z.object({
-    email: z.email(),
-    fullname: z.string().min(6),
-    password: z.string().min(6),
-});
--- src/libs/configs/config.data.ts
+++
@@ -1,1 +0,0 @@
-export const emailEndsWith = ["@gmail.com", "@hotmail.com", "@outlook.com"];
